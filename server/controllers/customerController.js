5e726b1bf9ccacb8834008ce46461fee55eb0b79
Siddhi <siddhishivale1@gmail.com>
2024-12-18 16:24:40 +0530

    Designed add user page

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -13,3 +13,19 @@ exports.homePage = async (req, res) => {
     res.render('index', locals);
 
 }
+
+/**
+ * Get /
+ * New customer form
+ */
+
+exports.addCustomer = async (req, res) => {
+    
+    const locals = {
+        title: 'Add New Customer - NodeJs',
+        description: 'Free NodeJs User Management System'
+    }
+    
+    res.render('customer/add', locals);
+
+}
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const customerController = require('../controllers/customerController');
 
 router.get('/', customerController.homePage);
+router.get('/add', customerController.addCustomer);
 
 module.exports = router;
